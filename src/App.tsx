69ee54a19ea60939953244d73689c03a345fb0bb
Anaflavia <anaflaviadmar@gmail.com>
2021-04-24 12:19:43 -0500

    feat: add crecemas/components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button } from '@crecemas/components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Button>Holi</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
